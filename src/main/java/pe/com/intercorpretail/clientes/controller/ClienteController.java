05943201e621ff5dd0d732324877094eab87d594
Marcos Aguirre <maguirre@kolektor.com.ar>
2020-01-23 15:25:38 -0300

    Agregamos swagger

--- src/main/java/pe/com/intercorpretail/clientes/controller/ClienteController.java
+++ src/main/java/pe/com/intercorpretail/clientes/controller/ClienteController.java
@@ -1,13 +1,14 @@
 package pe.com.intercorpretail.clientes.controller;
 
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClienteController {
 
-
+    @ApiOperation(value = "Crear nuevo cliente",response=String.class)
     @GetMapping(value = "/creacliente")
     public String obtenerHolaMunto(){
         return "Hola Mundo";
