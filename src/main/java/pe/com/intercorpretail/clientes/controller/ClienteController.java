0eff7acb9d58f4bd90788e87241e7b7c73ab80c1
Marcos Aguirre <maguirre@kolektor.com.ar>
2020-01-23 16:46:44 -0300

    Guardamos en la base

--- src/main/java/pe/com/intercorpretail/clientes/controller/ClienteController.java
+++ src/main/java/pe/com/intercorpretail/clientes/controller/ClienteController.java
@@ -2,15 +2,23 @@ package pe.com.intercorpretail.clientes.controller;
 
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pe.com.intercorpretail.clientes.dto.ClienteDto;
+import pe.com.intercorpretail.clientes.service.ClienteService;
 
 @RestController
 public class ClienteController {
 
+    @Autowired
+    ClienteService clienteService;
+
     @ApiOperation(value = "Crear nuevo cliente",response=String.class)
     @GetMapping(value = "/creacliente")
-    public String crearClientes(){
+    public String crearClientes(@RequestBody ClienteDto clienteDto){
+
         return "Hola Mundo";
     }
 }
--- src/main/java/pe/com/intercorpretail/clientes/service/ClienteService.java
+++ src/main/java/pe/com/intercorpretail/clientes/service/ClienteService.java
@@ -1,4 +1,22 @@
 package pe.com.intercorpretail.clientes.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import pe.com.intercorpretail.clientes.dao.ClienteDao;
+import pe.com.intercorpretail.clientes.dto.ClienteDto;
+import pe.com.intercorpretail.clientes.repository.ClienteRepository;
+
 public class ClienteService {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    public void insertCliente(ClienteDto clienteDto){
+        ClienteDao clienteDao = new ClienteDao();
+        clienteDao.setNombre(clienteDto.getNombre());
+        clienteDao.setApellido(clienteDto.getApellido());
+        clienteDao.setEdad(clienteDto.getEdad());
+        clienteDao.setFechaNacimiento(clienteDto.getFechaNacimiento());
+        clienteRepository.save(clienteDao);
+
+    }
 }
